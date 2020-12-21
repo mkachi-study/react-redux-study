dfe0ad2c23a62725e26917f8da56f4d19b9aa0be
MKachi <mkachi@naver.com>
2020-12-21 12:36:45 +0900

    feat: CheckBox component 추가

---
+++ src/components/atoms/CheckBox/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import classNames from 'classnames'
+import styles from './style.module.css'
+
+import { FaCheck } from 'react-icons/fa'
+
+interface IProps {
+  className?: string
+  checked: boolean
+  onChange?(event: React.FormEvent<HTMLInputElement>): void
+}
+
+const CheckBox: React.FC<IProps> = ({ className, checked, onChange = () => { } }) => {
+  console.log('render')
+  const classProps = classNames(className, styles['default'])
+  return <span className={classProps}>
+    <span className={styles['box']}>
+      {checked && <FaCheck className={styles['icon']} />}
+    </span>
+    <input className={styles['input']} type={'checkbox'} checked={checked} onChange={onChange} />
+  </span>
+}
+
+export default CheckBox
