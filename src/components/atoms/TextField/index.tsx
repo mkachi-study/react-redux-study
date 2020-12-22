e195e5b772deba9e3a67863e723e62c82b68a6b8
MKachi <mkachi@naver.com>
2020-12-22 14:34:21 +0900

    feat: TextField component error text css 수정

--- src/components/atoms/TextField/index.tsx
+++ src/components/atoms/TextField/index.tsx
@@ -18,12 +18,13 @@ const TextField: React.FC<IProps> = ({ className, error = false, errorText = '',
   const classProps = classNames(className, styles['default'])
   const lineProps = classNames(styles['line-normal'], isFocus ? styles['line-focus'] : '', error ? styles['line-error'] : '')
   const inputProps = classNames(styles['input'], error ? styles['input-error'] : styles['input-normal'])
+  const errorProps = classNames(styles['error-text'], (errorText !== '' && error) ? styles['error-text-show'] : '')
 
   return <div className={classProps}>
     <span className={lineProps}>
       <input className={inputProps} type={type} onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} placeholder={placeholder} value={value} onChange={onChange} />
     </span>
-    {(errorText !== '' && error) && <span className={styles['error-text']}>{errorText}</span>}
+    <span className={errorProps}>{errorText}</span>
   </div>
 }
 
