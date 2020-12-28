3697c8fd3de5a07df38615f8c422fdda9449432d
MKachi <mkachi@naver.com>
2020-12-29 02:41:16 +0900

    feat: TodoItem key 값 추가

--- src/components/organisms/TodoList/index.tsx
+++ src/components/organisms/TodoList/index.tsx
@@ -16,7 +16,7 @@ const TodoList: React.FC<IProps> = ({ className, items }) => {
   const classProps = classNames(className, styles['default'])
   return <Flex className={classProps} direction={'column'}>
     {items.map((item, index) => {
-      return <TodoItem subject={item.subject} checked={item.checked} onRemove={() => {
+      return <TodoItem key={index} subject={item.subject} checked={item.checked} onRemove={() => {
         console.log('onRemove')
       }} onChange={() => {
         console.log('onChange')
