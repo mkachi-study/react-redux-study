aa1d5ec691d19b228e330247add2b86ae6744d1e
MKachi <mkachi@naver.com>
2021-01-03 05:41:04 +0900

    feat: TodoList scroll 관련 style 추가

--- src/components/organisms/TodoList/index.tsx
+++ src/components/organisms/TodoList/index.tsx
@@ -15,7 +15,7 @@ interface IProps {
 const TodoList: React.FC<IProps> = ({ className, items }) => {
   const classProps = classNames(className, styles['default'])
   return (
-    <Flex className={classProps} direction={'column'}>
+    <Flex className={classProps} direction={'column'} wrap={'no-wrap'} overflow={'scroll'}>
       {items.map((item, index) => {
         return (
           <TodoItem
