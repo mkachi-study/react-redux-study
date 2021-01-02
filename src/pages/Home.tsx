f62d3023a8100431ad9764dc3f4cd720cce0184b
MKachi <mkachi@naver.com>
2021-01-03 05:41:57 +0900

    feat: Home page 작성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,43 @@
 import React from 'react'
 
+import Container from '../components/layouts/Container'
+import TodoList from '../components/organisms/TodoList'
+
 const Home = () => {
-  return <p>{'Home'}</p>
+  return (
+    <>
+      <TodoList
+        items={[
+          { checked: true, subject: 'hello1' },
+          { checked: false, subject: 'hello2' },
+          { checked: false, subject: 'hello3' },
+          { checked: true, subject: 'hello4' },
+          { checked: true, subject: 'hello5' },
+          { checked: false, subject: 'hello3' },
+          { checked: true, subject: 'hello4' },
+          { checked: true, subject: 'hello5' },
+          { checked: false, subject: 'hello3' },
+          { checked: true, subject: 'hello4' },
+          { checked: true, subject: 'hello5' },
+          { checked: false, subject: 'hello3' },
+          { checked: true, subject: 'hello4' },
+          { checked: true, subject: 'hello5' },
+          { checked: false, subject: 'hello3' },
+          { checked: true, subject: 'hello4' },
+          { checked: false, subject: 'hello3' },
+          { checked: true, subject: 'hello4' },
+          { checked: true, subject: 'hello5' },
+          { checked: true, subject: 'hello5' },
+          { checked: false, subject: 'hello6' }
+        ]}
+      />
+      <p>
+        <p>{'test1'}</p>
+        <p>{'test2'}</p>
+        <p>{'test3'}</p>
+      </p>
+    </>
+  )
 }
 
 export default Home
