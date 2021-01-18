bbb4ef3256bff835f1c24a8579ff592b88861895
MKachi <mkachi@naver.com>
2021-01-18 15:39:01 +0900

    feat: onRemove, onChange callback 추가

--- src/components/organisms/TodoList/index.tsx
+++ src/components/organisms/TodoList/index.tsx
@@ -10,26 +10,16 @@ import TodoItem from '../../molecules/TodoItem'
 interface IProps {
   className?: string
   items: ITodo[]
+  onRemove(item: ITodo): void
+  onChange(item: ITodo): void
 }
 
-const TodoList: React.FC<IProps> = ({ className, items }) => {
+const TodoList: React.FC<IProps> = ({ className, items, onRemove, onChange }) => {
   const classProps = classNames(className, styles['default'])
   return (
     <Flex className={classProps} direction={'column'} wrap={'no-wrap'} overflow={'scroll'}>
       {items.map((item, index) => {
-        return (
-          <TodoItem
-            key={index}
-            subject={item.subject}
-            checked={item.checked}
-            onRemove={() => {
-              console.log('onRemove')
-            }}
-            onChange={() => {
-              console.log('onChange')
-            }}
-          />
-        )
+        return <TodoItem key={index} subject={item.subject} checked={item.checked} onRemove={() => onRemove(item)} onChange={() => onChange(item)} />
       })}
     </Flex>
   )
