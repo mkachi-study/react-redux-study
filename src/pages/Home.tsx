8ffe0981232dc9d0899b4d395460fe2b71186e19
MKachi <mkachi@naver.com>
2021-01-18 15:56:18 +0900

    feat: home page 작성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import Container from '../components/layouts/Container'
 import TodoList from '../components/organisms/TodoList'
+import AddPanel from '../components/organisms/AddPanel'
+import { useDispatch, useSelector } from 'react-redux'
+import { StoreState } from 'src/store'
+import { fetchTodo, addTodo, removeTodo, checkTodo } from '../reducers/TodoList'
+import Modal from '../components/templates/Modal'
 
 const Home = () => {
+  const [input, setInput] = useState('')
+  const dispatch = useDispatch()
+  const { status, items } = useSelector((state: StoreState) => ({
+    status: state.todoList.status,
+    items: state.todoList.todos
+  }))
+
+  useEffect(() => {
+    dispatch(fetchTodo())
+  }, [])
+
   return (
     <>
-      <TodoList
-        items={[
-          { checked: true, subject: 'hello1' },
-          { checked: false, subject: 'hello2' },
-          { checked: false, subject: 'hello3' },
-          { checked: true, subject: 'hello4' },
-          { checked: true, subject: 'hello5' },
-          { checked: false, subject: 'hello3' },
-          { checked: true, subject: 'hello4' },
-          { checked: true, subject: 'hello5' },
-          { checked: false, subject: 'hello3' },
-          { checked: true, subject: 'hello4' },
-          { checked: true, subject: 'hello5' },
-          { checked: false, subject: 'hello3' },
-          { checked: true, subject: 'hello4' },
-          { checked: true, subject: 'hello5' },
-          { checked: false, subject: 'hello3' },
-          { checked: true, subject: 'hello4' },
-          { checked: false, subject: 'hello3' },
-          { checked: true, subject: 'hello4' },
-          { checked: true, subject: 'hello5' },
-          { checked: true, subject: 'hello5' },
-          { checked: false, subject: 'hello6' }
-        ]}
-      />
-      <p>
-        <p>{'test1'}</p>
-        <p>{'test2'}</p>
-        <p>{'test3'}</p>
-      </p>
+      {status === 'success' ? (
+        <>
+          <TodoList items={items} onRemove={item => dispatch(removeTodo(item.id))} onChange={item => dispatch(checkTodo(item.id, !item.checked))} />
+          <AddPanel
+            subject={input}
+            onChange={event => setInput(event.currentTarget.value)}
+            onAppend={() => {
+              if (input === '') {
+                return
+              }
+
+              dispatch(
+                addTodo({
+                  id: Date.now().toString(),
+                  checked: false,
+                  subject: input
+                })
+              )
+              setInput('')
+            }}
+          />
+        </>
+      ) : (
+        <Modal>{status === 'failed' ? '로드 실패' : '로딩중'}</Modal>
+      )}
     </>
   )
 }
